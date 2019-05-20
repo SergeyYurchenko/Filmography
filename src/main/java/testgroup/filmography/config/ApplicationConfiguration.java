07a4695e783e8fe550179c735c9f9240761c65f3
SergeyYurchenko <yurchenko10serhii@gmail.com>
2019-05-20 19:51:35 +0300

    Initial commit

---
+++ src/main/java/testgroup/filmography/config/ApplicationConfiguration.java
@@ -0,0 +1,9 @@
+package testgroup.filmography.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "testgroup.filmography")
+public class ApplicationConfiguration {
+}
